e6b9e203edaf4cf0654bf22a235c945b4f10628e
Mohamed Salem Kh <mohamedsalemkhyarhoum@gmail.com>
2026-05-25 23:39:59 +0100

    commit

--- migrations/env.py
+++ migrations/env.py
@@ -1,7 +1,6 @@
 from __future__ import with_statement
 import sys
 import os
-from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
@@ -9,8 +8,6 @@ from sqlalchemy import engine_from_config, pool
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 config = context.config
-if config.config_file_name:
-    fileConfig(config.config_file_name)
 
 try:
     from src.main import create_web_app
